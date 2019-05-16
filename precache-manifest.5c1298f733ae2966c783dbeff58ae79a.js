0014c9ec7029cfaab9bba43a8679fe8fd711d653
Subendra Sharma <engg.sharma.subendra.kr@mail.com>
2019-05-16 07:41:19 +0530

    build files

---
+++ precache-manifest.5c1298f733ae2966c783dbeff58ae79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d7368edb139208d4fabdb2a756b928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d14f2b97912a5c1de5",
+    "url": "/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "05ca86a6027dfd7f63ce",
+    "url": "/static/css/main.606d2d05.chunk.css"
+  },
+  {
+    "revision": "70d14f2b97912a5c1de5",
+    "url": "/static/js/2.0d691db9.chunk.js"
+  },
+  {
+    "revision": "05ca86a6027dfd7f63ce",
+    "url": "/static/js/main.996d6077.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
